c44d891fdd3fd0aa9004d3f048a886fc73055122
Wolfgang Gassler <wolfgang@gassler.org>
2023-04-20 11:48:41 +0200

    docker support (#1)
    * docker support
    
    Co-authored-by: Matthias Endler <matthias@endler.dev>

---
+++ Makefile
@@ -0,0 +1,23 @@
+shell := /bin/bash
+
+COMPOSE = docker compose
+
+.PHONY: help
+help: ## help message, list all commands
+	@echo -e "$$(grep -hE '^\S+:.*##' $(MAKEFILE_LIST) | sed -e 's/:.*##\s*/:/' -e 's/^\(.\+\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' | column -c2 -t -s :)"
+
+.PHONY: build
+build: ## build docker image
+	$(COMPOSE) build
+
+.PHONY: up
+up: ## start docker container
+	$(COMPOSE) up
+
+.PHONY: shell-%
+shell-%: ## run shell in container
+	$(COMPOSE) exec $* $(shell)
+
+.PHONY: test-request
+test-request: ## send test request to webhook
+	curl http://localhost:8080/update-Nohn0lahGh5ahnaeng9Xolaewu2fae
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { exec } = require('child_process');
+
+// define environment variables
+const PORT = process.env.PORT || 3000;
+const HOOK = process.env.HOOK || '/hook123456789';
+
+// create express app
+const app = express();
+
+let updateRunning = false;
+
+app.get(HOOK, (req, res) => {
+  // avoid to start multiple concurrent updates
+  if (!updateRunning) {
+    updateRunning = true;
+    exec('./update.sh', (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(stdout);
+      }
+      updateRunning = false;
+    })
+    res.status(200).send('Update started');
+  } else {
+    res.status(429).send('Update already running');
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+  console.log(`Waiting for hook on ${HOOK}`)
+})
